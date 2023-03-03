72510b2469f10caa7c876bb22663cd19701d1368
arnoud35 <ravelosaona35@gmail.com>
2023-03-03 18:49:55 +0300

    ajout de todo

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.models.user_model import User
+from app.models.todo_model import Todo
 from app.api.api_v1.router import router
 
 app = FastAPI(
@@ -31,6 +32,7 @@ async def app_init():
         database=db_client,
         document_models= [
             User,
+            Todo
         ]
     )
 
